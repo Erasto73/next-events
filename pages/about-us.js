7e5b22f42e594b75e1c233030dc48ec501e6cc6f
Erasto73 <erastochina@gmail.com>
2023-08-24 13:12:58 +0300

    first commit

---
+++ pages/about-us.js
@@ -0,0 +1,42 @@
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1> About us Page</h1>
+      <p>
+        Want to travel in Europe and know about the best events? Please find our selection of the best
+         European events. Discover the best music festivals in Europe, the best sports events but also 
+         the best traditional and folks events, the top religious events, the best and most popular 
+         festivals, the biggest outdoor events and the best art events. Discover our selection of the 
+         top events in Europe and get ready to book your plane/train or to jump into a bus/tram/car to 
+         enjoy them!
+      </p>
+      <h2>Our works </h2>
+      <p>
+        We have been organising policy conferences in Brussels and around Europe since 1989. Whether 
+        working on our own events or carefully curating yours, we establish key connections and promote 
+        understanding of topical policy issues and legislation.
+      </p>
+      <ul className="about_us_list">
+        <li>
+          <h3>Achivement </h3>
+          <p>
+            Our reputation is built on the delivery of effective, meticulously planned events that
+            provide unique insights from the people behind the policy. Working closely with key 
+            stakeholders from all sides, our expert team develop conference programmes with impact, 
+            and provide first-class event logistics.
+          </p>
+        </li>
+        <li>
+          <h3>Our Team </h3>
+          <p>
+            Utilising our network of trusted contacts from across business, policy, not-for-profits 
+            and the press, we build conference programmes that bring together the full spectrum of 
+            ideas and positions to form a neutral platform for debate.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <div className="topNav">
+          <h2><Image alt="logo" src={'/images/logo_black.png'} width={200} height={100}/>Signaxcom</h2>        
+          <nav>
+            <ul>
+              <li>
+                <Link href="/" passHref>
+                  <a> Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/events" passHref>
+                  <a> Events</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about-us" passHref>
+                  <a> About us</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <p className="title"> UP COMING EVENTS IN 2023 </p>
+      </div>
+    </header>
+  );
+};
